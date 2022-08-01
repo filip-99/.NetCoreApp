cd6c1014bee0a6130c47499768f3bdd8ae54d6ab
Filip Radosavljevic <filip6331@gmail.com>
2022-08-01 18:24:42 +0200

    Kreiran folder ViewModels i unutar njega klasa BookVM.cs, sa podacima koje korisnik treba da unese

---
+++ my-books/Data/ViewModels/BookVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace my_books.Data.ViewModels
+{
+    // BookVM je ViewModel klasa koja sadrži deo podataka iz model klase Book.cs
+    // Podaci u ovoj klasi su oni koje unosi korisnik, zato je izostavljen Id jer je identity i recimo datum unosa knjige u bazu, jer se uzima trenutni datum i korisnik ga ne unosi
+    public class BookVM
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public string Genre { get; set; }
+        public string Author { get; set; }
+        public int? Rate { get; set; }
+        public string Cover { get; set; }
+    }
+}
