c391d59b3e3854e324db98510e5a8992bbe2d155
Filip Radosavljevic <filip6331@gmail.com>
2022-08-03 16:39:40 +0200

    Kreirana model klasa Publisher sa atributima i svojstvom koje ukazuje na odnos sa modelom klase Book

---
+++ my-books/Data/Models/Publisher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace my_books.Data.Models
+{
+    // Kreirali smo model klasu za Izdavača
+    public class Publisher
+    {
+        // Najpre definišemo atribute, koji će predstavljati kolone za tabelu Publishers
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // Nakon toga definišemo svojstva tj. odnos između modela Publisher i Book
+        // Kreiramo atribut tipa List koji sadrži više knjiga, jer izdavač može imati više izdatih knjiga
+        public List<Book> Books { get; set; }
+    }
+}
