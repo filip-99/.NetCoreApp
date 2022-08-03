49f2be40bfa85d819714655ccd9d12e144b59b4c
Filip Radosavljevic <filip6331@gmail.com>
2022-08-03 22:00:09 +0200

    Izvršena migracija i dodati komentari kao objašnjenje, takođe dodate komande za migraciju i apdejt baze u fajlu NuGetPackageInstallList.txt

---
+++ my-books/Migrations/20220803194605_ManyToManyAdded.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace my_books.Migrations
+{
+    public partial class ManyToManyAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Publisher_PublisherId",
+                table: "Books");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Publisher",
+                table: "Publisher");
+            // Imamo promenu u nazivu, jer se model klasa zove Publisher, a tabela u bazi će se zvati Publishers. Ovo se dešava zato što smo tako naveli u AppDbContext.cs klasi
+            migrationBuilder.RenameTable(
+                name: "Publisher",
+                newName: "Publishers");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Publishers",
+                table: "Publishers",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    fullName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books_Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    AuthorId = table.Column<int>(type: "int", nullable: false)
+                },
+                // Ovde imamo ograničenja
+                constraints: table =>
+                {
+                    // Primarni ključ je Id
+                    table.PrimaryKey("PK_Books_Authors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Books_Authors_Authors_AuthorId",
+                        // U tabeli Books_Authors kolona AuthorId predstavlja FOREIGN KEY
+                        column: x => x.AuthorId,
+                        // Ta kolona AuthorId u Tabeli Authors
+                        principalTable: "Authors",
+                        // Odnosi se na Id
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Books_Authors_Books_BookId",
+                        // Isto važi i sa Books tabelom i Books_Authors
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_Authors_AuthorId",
+                table: "Books_Authors",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_Authors_BookId",
+                table: "Books_Authors",
+                column: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Publishers_PublisherId",
+                table: "Books",
+                column: "PublisherId",
+                principalTable: "Publishers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Publishers_PublisherId",
+                table: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Books_Authors");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Publishers",
+                table: "Publishers");
+
+            migrationBuilder.RenameTable(
+                name: "Publishers",
+                newName: "Publisher");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Publisher",
+                table: "Publisher",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Publisher_PublisherId",
+                table: "Books",
+                column: "PublisherId",
+                principalTable: "Publisher",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
