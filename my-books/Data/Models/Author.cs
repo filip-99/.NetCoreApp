2de27aaf5cf76fa080399fe1089efa963437d0f1
Filip Radosavljevic <filip6331@gmail.com>
2022-08-03 18:30:10 +0200

    Kreirana model klasa Author.cs sa atributima

---
+++ my-books/Data/Models/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace my_books.Data.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string fullName { get; set; }
+
+        // Dodajemo navigaciona svojstva, ali pre toga potrebno je da kreiramo zajednički model za Autora i Knjihu
+    }
+}
