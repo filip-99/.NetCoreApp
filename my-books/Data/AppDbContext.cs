3a7bf2bedc2b749f2e99cac3676157bfd998abd5
Filip Radosavljevic <filip6331@gmail.com>
2022-08-03 21:44:24 +0200

    U kontekst klasi AppDbContext konfigurisana sve 3 tabele i izvršena referenciranje svih model klasa sa budućim tabelama u bazi

--- my-books/Data/AppDbContext.cs
+++ my-books/Data/AppDbContext.cs
@@ -16,8 +16,36 @@ namespace my_books.Data
         {
 
         }
+
+        // Potrebno je sada da konfigurišemo ova 3 modela (Author.cs, Book.cs i Book_Author.cs) kako bi uspešno apdejtovali bazu
+        // Ovo se konfiguriše korišćenjem Fluent API-ja za Entity Framework Core
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Sada definišemo odnos između Book i Book_Author
+            modelBuilder.Entity<Book_Author>()
+                // HasOne - jedna Knjiga
+                .HasOne(b => b.Book)
+                // WithMany - Može imati više (više je na strani Book_Author)
+                .WithMany(ba => ba.Book_Authors)
+                // Sada definišemo javni ključ
+                .HasForeignKey(bi => bi.BookId);
+
+            // Sada isto ovako definišemo odnos između Author i Book_Author
+            modelBuilder.Entity<Book_Author>()
+                .HasOne(a => a.Author)
+                .WithMany(ba => ba.Book_Authors)
+                .HasForeignKey(ai => ai.AuthorId);
+        }
+
         // Sada vršimo referenciranje model klase sa tabelom u bazi podataka
         // Promenjiva će biti tipa DbSet i nazvaćemo je kao tabelu u bazi Books. Sada možemo manipulisati tabelom uz pomoć ovog naziva "Books"
         public DbSet<Book> Books { get; set; }
+
+        // Sada vršimo referenciranje modela klasa sa tabelama u bazi podataka, kao što je urađeno za Books
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book_Author> Books_Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+    
+
     }
 }
