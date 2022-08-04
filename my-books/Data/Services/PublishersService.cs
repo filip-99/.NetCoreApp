152191b7b5a26b1a33d0085c14e9c10a24484b22
Filip Radosavljevic <filip6331@gmail.com>
2022-08-04 18:07:24 +0200

    Kreirane su klase PublisherVM.cs i PublishersService.cs

---
+++ my-books/Data/Services/PublishersService.cs
@@ -0,0 +1,26 @@
+﻿using my_books.Data.Models;
+using my_books.Data.ViewModels;
+
+namespace my_books.Data.Services
+{
+    public class PublishersService
+    {
+        // Najpre dodajemo kontekst fail, da bi omogućili rad sa bazom
+        // Pošto koristimo Entity Framework Core zbog toga je ovakva struktura
+        private AppDbContext _context;
+        public PublishersService(AppDbContext context)
+        {
+            _context=context;
+        }
+
+        public void AddPublisher(PublisherVM publisher)
+        {
+            var _publisher = new Publisher()
+            {
+                Name = publisher.Name
+            };
+            _context.Publishers.Add(_publisher);
+            _context.SaveChanges();
+        }
+    }
+}
