410495d3956cf68c8d9112379f4697cad73a02b8
Filip Radosavljevic <filip6331@gmail.com>
2022-08-04 17:56:50 +0200

    Kreiran view model klasa AuthorVM.cs i servis klasa AuthorsService.cs

---
+++ my-books/Data/Services/AuthorsService.cs
@@ -0,0 +1,26 @@
+﻿using my_books.Data.Models;
+using my_books.Data.ViewModels;
+
+namespace my_books.Data.Services
+{
+    public class AuthorsService
+    {
+        // Najpre dodajemo kontekst fail, da bi omogućili rad sa bazom
+        // Pošto koristimo Entity Framework Core zbog toga je ovakva struktura
+        private AppDbContext _context;
+        public AuthorsService(AppDbContext context)
+        {
+            _context=context;
+        }
+
+        public void AddAuthor(AuthorVM author)
+        {
+            var _author = new Author()
+            {
+                FullName = author.FullName
+            };
+            _context.Authors.Add(_author);
+            _context.SaveChanges();
+        }
+    }
+}
