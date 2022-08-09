42627d7fd368e562dbadb6d1366fd5efa8738a82
Filip Radosavljevic <filip6331@gmail.com>
2022-08-09 15:14:52 +0200

    Izvršena nova migracija, nakom promena komandom Add-Migration BookAuthorColumnRemoved

---
+++ my-books/Migrations/20220809131356_BookAuthorColumnRemoved.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace my_books.Migrations
+{
+    public partial class BookAuthorColumnRemoved : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Books");
+
+            migrationBuilder.RenameColumn(
+                name: "fullName",
+                table: "Authors",
+                newName: "FullName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "Authors",
+                newName: "fullName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
