38f6801a44d53261a8b3c7ecec16585ad6c26b83
Filip Radosavljevic <filip6331@gmail.com>
2022-08-10 15:55:52 +0200

    U klasi AuthorsService.cs klasi kreirana metoda za prikaz svih knjiga od određenog autora

--- my-books/Data/Services/AuthorsService.cs
+++ my-books/Data/Services/AuthorsService.cs
@@ -1,5 +1,6 @@
 ﻿using my_books.Data.Models;
 using my_books.Data.ViewModels;
+using System.Linq;
 
 namespace my_books.Data.Services
 {
@@ -21,6 +22,23 @@ namespace my_books.Data.Services
             };
             _context.Authors.Add(_author);
             _context.SaveChanges();
+        }
+
+        public AuthorWithBooksVM GetAuthorWithBooks(int authorId)
+        {
+            // Promenjiva _author dobija sledeću vrednost:
+            // Pristupamo tabeli Authors, zadajemo uslov sa Where, parametar author se odnosi na tabelu celu, a author.Id se odnosi na kolonu
+            // Ako u koloni Id postoji vrednost == prosleđenom parametru u funkciji, onda selektuj ga
+            var _author = _context.Authors.Where(author => author.Id == authorId).Select(n => new AuthorWithBooksVM()
+            {
+                // Izvuči iz reda puno ime autora
+                FullName=n.FullName,
+                // Izvuči sve njegove knjige, tako što će da se pomoću promenjive Book_Authors uđe u klasu Book_Author
+                // I pošto ona među atribute sadrži i Book koji referencira na klasu Book, pomoću njega će mo ući u klasu
+                // Na ovaj način uzimamo naslove iz tabela, jer su klase povezane sa tabelama
+                BookTitles = n.Book_Authors.Select(n => n.Book.Title).ToList()
+            }).FirstOrDefault();
+            return _author;
         }
     }
 }
