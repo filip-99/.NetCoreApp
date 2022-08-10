a887a24fa828f1e28f482df3f697a64e4e997d87
Filip Radosavljevic <filip6331@gmail.com>
2022-08-10 15:02:03 +0200

    U klasi AuthorVM.cs dodata nova klasa, koja će nam koristiti za prikaz svih knjiga za određenog autora

--- my-books/Data/ViewModels/AuthorVM.cs
+++ my-books/Data/ViewModels/AuthorVM.cs
@@ -1,7 +1,16 @@
-﻿namespace my_books.Data.ViewModels
+﻿using System.Collections.Generic;
+
+namespace my_books.Data.ViewModels
 {
     public class AuthorVM
     {
         public string FullName { get; set; }
+    }
+
+    // Dodata nova klasa, koja će nam koristiti za prikaz svih knjiga za određenog autora
+    public class AuthorWithBooksVM
+    {
+        public string FullName { get; set; }
+        public List<string> BookTitles { get; set; }
     }
 }
